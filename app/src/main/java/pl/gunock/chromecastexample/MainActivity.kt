b28af80c50cea2895d10decb203c6db5d102b453
Gunock <asz.czyk@gmail.com>
2023-06-08 15:51:10 +0200

    Migrated input and buttons to Jetpack Compose

--- app/src/main/java/pl/gunock/chromecastexample/MainActivity.kt
+++ app/src/main/java/pl/gunock/chromecastexample/MainActivity.kt
@@ -1,108 +1,176 @@
 package pl.gunock.chromecastexample
 
 import android.os.Bundle
-import android.view.Menu
-import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.MenuItemCompat
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.cast.framework.CastContext
 import com.google.android.gms.cast.framework.CastSession
 import org.json.JSONObject
-import pl.gunock.chromecastexample.cast.CustomMediaRouteActionProvider
+import pl.gunock.chromecastexample.constants.CastConstants
 import pl.gunock.chromecastexample.enums.MoveAction
+import pl.gunock.chromecastexample.ui.theme.ChromecastExampleTheme
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var titleNamespace: String
-    private lateinit var moveNamespace: String
-    private lateinit var castContext: CastContext
-
-
-    private lateinit var textInput: EditText
-
-    private lateinit var sendButton: Button
-    private lateinit var leftButton: Button
-    private lateinit var rightButton: Button
-    private lateinit var upButton: Button
-    private lateinit var downButton: Button
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar_main))
-
-        titleNamespace = getString(R.string.chromecast_title_namespace)
-        moveNamespace = getString(R.string.chromecast_move_namespace)
-        castContext = CastContext.getSharedInstance()!!
-
-        textInput = findViewById(R.id.ed_text)
-
-        sendButton = findViewById(R.id.btn_send)
-        leftButton = findViewById(R.id.btn_left)
-        rightButton = findViewById(R.id.btn_right)
-        upButton = findViewById(R.id.btn_up)
-        downButton = findViewById(R.id.btn_down)
-
-        setupListeners()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-
-        val castActionProvider =
-            MenuItemCompat.getActionProvider(menu.findItem(R.id.menu_cast)) as CustomMediaRouteActionProvider
-
-        castActionProvider.routeSelector = castContext.mergedSelector!!
-
-        return true
-    }
-
-    private fun setupListeners() {
-        sendButton.setOnClickListener {
-            val session: CastSession = castContext.sessionManager.currentCastSession
-                ?: return@setOnClickListener
-
-            val text: String = textInput.text.toString()
-            val messageJson: JSONObject = JSONObject().apply { put("text", text) }
-            session.sendMessage(titleNamespace, messageJson.toString())
-        }
-
-        leftButton.setOnClickListener {
-            val session: CastSession = castContext.sessionManager.currentCastSession
-                ?: return@setOnClickListener
-
-            sendMoveActionCommand(session, MoveAction.LEFT)
-        }
-
-        rightButton.setOnClickListener {
-            val session: CastSession = castContext.sessionManager.currentCastSession
-                ?: return@setOnClickListener
-
-            sendMoveActionCommand(session, MoveAction.RIGHT)
-        }
-
-        upButton.setOnClickListener {
-            val session: CastSession = castContext.sessionManager.currentCastSession
-                ?: return@setOnClickListener
-
-            sendMoveActionCommand(session, MoveAction.UP)
-        }
-
-        downButton.setOnClickListener {
-            val session: CastSession = castContext.sessionManager.currentCastSession
-                ?: return@setOnClickListener
-
-            sendMoveActionCommand(session, MoveAction.DOWN)
+        setContent {
+            ChromecastExampleTheme {
+                ContentView()
+            }
         }
     }
+}
 
-    private fun sendMoveActionCommand(session: CastSession, moveAction: MoveAction) {
-        val messageJson: JSONObject = JSONObject().apply {
-            put("action", moveAction.toString())
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ContentView() {
+    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        TopAppBar(
+            title = { Text(stringResource(id = R.string.app_name)) },
+            colors = TopAppBarDefaults.largeTopAppBarColors(),
+            actions = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_cast_24),
+                        contentDescription = stringResource(id = R.string.menu_main_cast)
+                    )
+                }
+            })
+    }) { contentPadding ->
+        Column(
+            modifier = Modifier.padding(contentPadding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TitleCommandView()
+            Spacer(modifier = Modifier.height(20.dp))
+            MoveCommandView()
         }
-        session.sendMessage(moveNamespace, messageJson.toString())
     }
+}
 
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    ChromecastExampleTheme {
+        ContentView()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TitleCommandView() {
+    var text by rememberSaveable { mutableStateOf("") }
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        TextField(value = text, onValueChange = { text = it })
+        Spacer(modifier = Modifier.width(10.dp))
+        Button(onClick = { sendTitleCommand(text) }) {
+            Text(text = stringResource(R.string.main_activity_button_send))
+        }
+    }
+}
+
+@Composable
+fun MoveCommandView() {
+    Card(
+        elevation = CardDefaults.elevatedCardElevation()
+    ) {
+        Column(
+            modifier = Modifier.padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(modifier = Modifier.width(90.dp), onClick = {
+                sendMoveActionCommand(MoveAction.UP)
+            }) {
+                Text(text = stringResource(R.string.main_activity_button_up))
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(modifier = Modifier.width(90.dp), onClick = {
+                    sendMoveActionCommand(MoveAction.LEFT)
+                }) {
+                    Text(text = stringResource(R.string.main_activity_button_left))
+                }
+                Spacer(modifier = Modifier.width(20.dp))
+                Button(modifier = Modifier.width(90.dp), onClick = {
+                    sendMoveActionCommand(MoveAction.RIGHT)
+                }) {
+                    Text(text = stringResource(R.string.main_activity_button_right))
+                }
+            }
+
+            Button(modifier = Modifier.width(90.dp), onClick = {
+                sendMoveActionCommand(MoveAction.DOWN)
+            }) {
+                Text(text = stringResource(R.string.main_activity_button_down))
+            }
+        }
+    }
+}
+
+//override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//    menuInflater.inflate(R.menu.menu_main, menu)
+//
+//    val castActionProvider =
+//        MenuItemCompat.getActionProvider(menu.findItem(R.id.menu_cast)) as CustomMediaRouteActionProvider
+//
+//    castActionProvider.routeSelector = castContext.mergedSelector!!
+//
+//    return true
+//}
+
+private fun sendTitleCommand(text: String) {
+    val session: CastSession =
+        CastContext.getSharedInstance()?.sessionManager?.currentCastSession ?: return
+
+    val messageJson: JSONObject = JSONObject().apply {
+        put("text", text)
+    }
+    session.sendMessage(CastConstants.TITLE_NAMESPACE, messageJson.toString())
+}
+
+private fun sendMoveActionCommand(moveAction: MoveAction) {
+    val session: CastSession =
+        CastContext.getSharedInstance()?.sessionManager?.currentCastSession ?: return
+
+    val messageJson: JSONObject = JSONObject().apply {
+        put("action", moveAction.toString())
+    }
+    session.sendMessage(CastConstants.MOVE_NAMESPACE, messageJson.toString())
 }
---
+++ app/src/main/java/pl/gunock/chromecastexample/constants/CastConstants.kt
@@ -0,0 +1,6 @@
+package pl.gunock.chromecastexample.constants
+
+object CastConstants {
+    const val TITLE_NAMESPACE = "urn:x-cast:example.cast.title"
+    const val MOVE_NAMESPACE = "urn:x-cast:example.cast.move"
+}
